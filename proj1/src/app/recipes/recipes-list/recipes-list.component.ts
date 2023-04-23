a8b89b1ceba8cccef08751be25e09f2e349d67c4
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2023-04-23 13:59:09 +0530

    completed section-3 basic proj setup

--- proj1/src/app/recipes/recipes-list/recipes-list.component.ts
+++ proj1/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipes } from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent {
-
+  recipes: Recipes[] = [
+    new Recipes('A Test', 'This is simply a test', 'https://img.freepik.com/premium-photo/cheese-vada-pav-grated-cheese-wada-pao-popular-bombay-snack-food_466689-69411.jpg?w=740'),
+    new Recipes('A Test', 'This is simply a test', 'https://img.freepik.com/premium-photo/cheese-vada-pav-grated-cheese-wada-pao-popular-bombay-snack-food_466689-69411.jpg?w=740')
+  ]
 }
--- proj1/src/app/recipes/recipes.component.ts
+++ proj1/src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
